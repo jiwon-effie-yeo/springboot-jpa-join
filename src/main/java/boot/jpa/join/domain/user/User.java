b78f12018ab167f3490aeddaf8bf31b8e9f34aa2
DESKTOP-A828S0O\jiwon <C:\Users\\Documents\git_SourceTree>
2019-12-15 16:28:45 +0900

    Create Domain Package - User, Post, Comment

---
+++ src/main/java/boot/jpa/join/domain/user/User.java
@@ -0,0 +1,38 @@
+package boot.jpa.join.domain.user;
+
+import boot.jpa.join.domain.comment.Comment;
+import boot.jpa.join.domain.post.Post;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Post> posts = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Comment> comments = new ArrayList<>();
+
+    @Builder
+    public User(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+}
